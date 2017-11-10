662d71e70d57bd45f059e7120b1b5cbc082c6515
dant-ke <asmartchoize@gmail.com>
2017-11-10 16:39:50 +0300

    2

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171110122920) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.text "body"
